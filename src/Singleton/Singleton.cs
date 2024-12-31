7f13c8c0885e603a4210505b5366ec79406f8154
Matin_Ghanbari <ghanbarimatin6@gmail.com>
2024-12-31 19:01:36 +0330

    Add singleton pattern

---
+++ src/Singleton/Singleton.cs
@@ -0,0 +1,23 @@
+﻿public class Singleton
+{
+    private static Singleton _instance;
+
+    private Singleton() { }
+
+    public static Singleton Instance
+    {
+        get
+        {
+            if (_instance == null)
+            {
+                _instance = new Singleton();
+            }
+            return _instance;
+        }
+    }
+
+    public void ShowMessage()
+    {
+        Console.WriteLine("Hello from Singleton!");
+    }
+}
